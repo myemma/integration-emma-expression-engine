428124299707087fbac76369dd434409dda34c97
Diya <diya.john@empressem.in>
2012-12-10 14:08:43 +0000

    Fixed styling issue

--- views/emma_subscribe_form.php
+++ views/emma_subscribe_form.php
@@ -5,14 +5,14 @@
     <div> <span style="color:<?php echo $message_color?>"><?php echo $message?></span>
 <?php }  ?>
 <style type="text/css">
+        #emma_subscribe_form { padding: 4px; margin: 0;  }
+        #emma_subscribe_form label { padding: 4px 0; text-align: left; }
         #emma_subscribe_form input[type="submit"] { background-color: #ECECEC; border: 1px solid #B4B4B4; text-transform: uppercase; padding: 4px 7px; margin: 7px 0 5px 0; text-align: center; }
         #emma_subscribe_form input[type="submit"]:hover { background-color: #CECECE; }
-        #emma-form-elements { padding: 4px; margin: 0;  }
-        #emma-form-elements .emma-group-list-label { padding: 4px 0; text-align: left; }
-        #emma-form-elements .emma-select-group-list { padding: 4px 0; }
-        #emma-form-elements input[type="text"] { border: 2px solid; color: #565656; width: 98%; padding: 4px; }
-        #emma-form-elements select { border: 1px solid #DCDCDC; color: #565656; width: 98%; padding: 4px; }
-        #emma-form-elements select option { padding: 2px 0; }
+        #emma_subscribe_form input[type="text"] { border: 1px solid; color: #565656; width: 95%; padding: 4px; }
+        #emma_subscribe_form input[type="email"] { border: 1px solid; color: #565656; width: 95%; padding: 4px; }
+        #emma_subscribe_form select { border: 1px solid #DCDCDC; color: #565656; width: 98%; padding: 4px; }
+        #emma_subscribe_form select option { padding: 2px 0; }
 </style>
 <?php
 
@@ -21,8 +21,9 @@
     $tmpl = array ( 'table_open'  => '<table width="100%" border="1" cellpadding="4px" cellspacing="1"' );
     $this->table->set_template($tmpl);
     $this->table->add_row(array(
-        '<label>Name</label>','<input type="text" name="name" id="name" style="color: #565656; width: 95%; padding: 4px;" />'));
-    $this->table->add_row(array('<label>Email*</label>','<input type="email" name="email" id="email" required="true" style="color: #565656; width: 95%; padding: 4px;" />'));
+        '<label>Name</label>','<input type="text" name="name" id="name"/>'));
+    //style="color: #565656; width: 95%; padding: 4px;" />'));
+    $this->table->add_row(array('<label>Email*</label>','<input type="email" name="email" id="email" required="true" />'));
     echo $this->table->generate();
     #echo form_hidden('emma_member_id', $member_id, 'class="field"');
     
